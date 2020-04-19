cc0752de083eaacfadb7e2ed1dc4a5052b49a28f
marsonshine <ms27946@outlook.com>
2020-04-19 22:26:54 +0800

    refactor: 重构，增加反射帮助类

--- src/MS.MicroService.MongoDb/MongoDbContextModelSource.cs
+++ src/MS.MicroService.MongoDb/MongoDbContextModelSource.cs
@@ -1,44 +1,39 @@
-﻿using MongoDB.Driver;
-using MS.Microservice.Domain;
-using System;
+﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection;
+using MongoDB.Driver;
+using MS.Microservice.Core.Reflection;
+using MS.Microservice.Domain;
 
-namespace MS.MicroService.MongoDb
-{
-    public class MongoDbContextModelSource : IMongoDbContextModelSource
-    {
+namespace MS.MicroService.MongoDb {
+    public class MongoDbContextModelSource : IMongoDbContextModelSource {
         protected readonly ConcurrentDictionary<Type, MongoDbContextModel> ModelCache = new ConcurrentDictionary<Type, MongoDbContextModel>();
 
-        public MongoDbContextModel GetModel(MongoDbContext dbContext)
-        {
+        public MongoDbContextModel GetModel(MongoDbContext dbContext) {
             return ModelCache.GetOrAdd(
                 dbContext.GetType(),
                 _ => CreateModel(dbContext)
             );
         }
 
-        protected virtual MongoDbContextModel CreateModel(MongoDbContext dbContext)
-        {
+        protected virtual MongoDbContextModel CreateModel(MongoDbContext dbContext) {
             var entityModels = BuildMongoEntityModelCollection(dbContext.GetType());
             return new MongoDbContextModel(entityModels);
         }
 
-        private IReadOnlyDictionary<Type, IMongoEntityModel> BuildMongoEntityModelCollection(Type dbContextType)
-        {
+        private IReadOnlyDictionary<Type, IMongoEntityModel> BuildMongoEntityModelCollection(Type dbContextType) {
             var collectionProperties =
                 from property in dbContextType.GetTypeInfo().GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                where IsAssignableToGenericType(property.PropertyType, typeof(IMongoCollection<>)) &&
-                 typeof(BaseEntity).IsAssignableFrom(property.PropertyType.GenericTypeArguments[0])
-                select property;
+            where ReflectionHelper.IsAssignableToGenericType(property.PropertyType, typeof(IMongoCollection<>)) &&
+                typeof(BaseEntity).IsAssignableFrom(property.PropertyType.GenericTypeArguments[0])
+            select property;
 
             var models = new Dictionary<Type, IMongoEntityModel>();
 
-            foreach (var collectionProperty in collectionProperties)
-            {
+            foreach (var collectionProperty in collectionProperties) {
                 var entityType = collectionProperty.PropertyType.GenericTypeArguments[0];
                 var collectionAttribute = collectionProperty.GetCustomAttributes().OfType<MongoCollectionAttribute>().FirstOrDefault();
 
@@ -46,32 +41,6 @@ namespace MS.MicroService.MongoDb
             }
 
             return new ReadOnlyDictionary<Type, IMongoEntityModel>(models);
-        }
-
-        // 待重构到一个单独的类中
-        public bool IsAssignableToGenericType(Type givenType, Type genericType)
-        {
-            var givenTypeInfo = givenType.GetTypeInfo();
-
-            if (givenTypeInfo.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
-            {
-                return true;
-            }
-
-            foreach (var interfaceType in givenTypeInfo.GetInterfaces())
-            {
-                if (interfaceType.GetTypeInfo().IsGenericType && interfaceType.GetGenericTypeDefinition() == genericType)
-                {
-                    return true;
-                }
-            }
-
-            if (givenTypeInfo.BaseType == null)
-            {
-                return false;
-            }
-
-            return IsAssignableToGenericType(givenTypeInfo.BaseType, genericType);
         }
     }
 }
