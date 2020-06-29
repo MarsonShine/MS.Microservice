f366958637d1674a4141552068667b3373d36713
marsonshine <ms27946@outlook.com>
2020-06-29 14:59:35 +0800

    feat: 泛型枚举

--- src/MS.Microservice.Domain/Enumerations/Enumeration.cs
+++ src/MS.Microservice.Domain/Enumerations/Enumeration.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace MS.Microservice.Domain.Enumerations
@@ -119,6 +121,159 @@ namespace MS.Microservice.Domain.Enumerations
         public static bool operator >=(Enumeration left, Enumeration right)
         {
             return ReferenceEquals(left, null) ? ReferenceEquals(right, null) : left.CompareTo(right) >= 0;
+        }
+    }
+
+    /// <summary>
+    /// https://github.com/HeadspringLabs/Enumeration/blob/master/Enumeration.cs
+    /// </summary>
+    /// <typeparam name="TEnumeration"></typeparam>
+    [Serializable]
+    [DebuggerDisplay("{DisplayName} - {Value}")]
+    public abstract class Enumeration<TEnumeration> : Enumeration<TEnumeration, int>
+        where TEnumeration : Enumeration<TEnumeration>
+    {
+        protected Enumeration(int value, string displayName)
+            : base(value, displayName)
+        {
+        }
+
+        public static TEnumeration FromInt32(int value)
+        {
+            return FromValue(value);
+        }
+
+        public static bool TryFromInt32(int listItemValue, out TEnumeration result)
+        {
+            return TryParse(listItemValue, out result);
+        }
+    }
+
+    public abstract class Enumeration<TEnumeration, TValue> : IComparable<TEnumeration>, IEquatable<TEnumeration>
+        where TEnumeration : Enumeration<TEnumeration, TValue>
+        where TValue : IComparable
+    {
+        private static readonly Lazy<TEnumeration[]> Enumerations = new Lazy<TEnumeration[]>(GetEnumerations);
+
+        [DataMember(Order = 1)]
+        readonly string _displayName;
+
+        [DataMember(Order = 0)]
+        readonly TValue _value;
+
+        protected Enumeration(TValue value, string displayName)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            _value = value;
+            _displayName = displayName;
+        }
+
+        public TValue Value
+        {
+            get { return _value; }
+        }
+
+        public string DisplayName
+        {
+            get { return _displayName; }
+        }
+
+        public int CompareTo(TEnumeration other)
+        {
+            return Value.CompareTo(other == default(TEnumeration) ? default : other.Value);
+        }
+
+        public override sealed string ToString()
+        {
+            return DisplayName;
+        }
+
+        public static TEnumeration[] GetAll()
+        {
+            return Enumerations.Value;
+        }
+
+        private static TEnumeration[] GetEnumerations()
+        {
+            Type enumerationType = typeof(TEnumeration);
+            return enumerationType
+                .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly)
+                .Where(info => enumerationType.IsAssignableFrom(info.FieldType))
+                .Select(info => info.GetValue(null))
+                .Cast<TEnumeration>()
+                .ToArray();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as TEnumeration);
+        }
+
+        public bool Equals(TEnumeration other)
+        {
+            return other != null && ValueEquals(other.Value);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public static bool operator ==(Enumeration<TEnumeration, TValue> left, Enumeration<TEnumeration, TValue> right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Enumeration<TEnumeration, TValue> left, Enumeration<TEnumeration, TValue> right)
+        {
+            return !Equals(left, right);
+        }
+
+        public static TEnumeration FromValue(TValue value)
+        {
+            return Parse(value, "value", item => item.Value.Equals(value));
+        }
+
+        public static TEnumeration Parse(string displayName)
+        {
+            return Parse(displayName, "display name", item => item.DisplayName == displayName);
+        }
+
+        static bool TryParse(Func<TEnumeration, bool> predicate, out TEnumeration result)
+        {
+            result = GetAll().FirstOrDefault(predicate);
+            return result != null;
+        }
+
+        private static TEnumeration Parse(object value, string description, Func<TEnumeration, bool> predicate)
+        {
+
+            if (!TryParse(predicate, out TEnumeration result))
+            {
+                string message = string.Format("'{0}' is not a valid {1} in {2}", value, description, typeof(TEnumeration));
+                throw new ArgumentException(message, "value");
+            }
+
+            return result;
+        }
+
+        public static bool TryParse(TValue value, out TEnumeration result)
+        {
+            return TryParse(e => e.ValueEquals(value), out result);
+        }
+
+        public static bool TryParse(string displayName, out TEnumeration result)
+        {
+            return TryParse(e => e.DisplayName == displayName, out result);
+        }
+
+        protected virtual bool ValueEquals(TValue value)
+        {
+            return Value.Equals(value);
         }
     }
 }
