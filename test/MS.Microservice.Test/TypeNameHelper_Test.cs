6c56609accdd27838b9d73269636c6b5e053239b
marsonshine <ms27946@outlook.com>
2020-09-27 11:53:17 +0800

    feat: typenamehelper 帮助类

---
+++ test/MS.Microservice.Test/TypeNameHelper_Test.cs
@@ -0,0 +1,32 @@
+﻿using MS.Infrastructure.Util.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using static MS.Microservice.Test.TypeNameHelper_Test.NestedType<string>;
+
+namespace MS.Microservice.Test
+{
+    public class TypeNameHelper_Test
+    {
+        [Fact]
+        public void DisplayTypeName_Test()
+        {
+            var name = TypeNameHelper.GetTypeDisplayName(this);
+            var name1 = TypeNameHelper.GetTypeDisplayName(typeof(NestedType<string>));
+            var name2 = TypeNameHelper.GetTypeDisplayName(typeof(ChildClassNestedType<string,int>));
+            Assert.NotEmpty(name);
+        }
+
+        public class NestedType<T>
+        {
+            public List<T> List { get; set; } = null!;
+
+            public class ChildClassNestedType<TChild, D>
+                where TChild : notnull
+            {
+                public Dictionary<TChild, D> HashDic { get; set; } = null!;
+            }
+        }
+    }
+}
