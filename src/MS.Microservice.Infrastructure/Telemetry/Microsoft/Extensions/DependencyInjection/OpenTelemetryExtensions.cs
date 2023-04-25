a0433e469c42c5b8bc30c066230126ca9c217ef4
marsonshine <ms27946@163.com>
2023-04-25 11:42:31 +0800

    feat: integrade open telemetry; jaeger

---
+++ src/MS.Microservice.Infrastructure/Telemetry/Microsoft/Extensions/DependencyInjection/OpenTelemetryExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace MS.Microservice.Infrastructure.Telemetry.Microsoft.Extensions.DependencyInjection
+{
+    public static class OpenTelemetryExtensions
+    {
+        public static IServiceCollection AddMsOpenTelemetry(this IServiceCollection services)
+        {
+            services.AddOpenTelemetry()
+                .ConfigureResource(resourceBuilder =>
+                {
+                    // TODO: 配置化
+                    resourceBuilder.AddService("Fz.OrderPlatform.Admin in OTel Service");
+                })
+                .WithTracing(cfg =>
+                {
+                    // TODO: 配置化
+                    cfg.AddSource("Fz.OrderPlatform.Admin in OTel Source")
+                    .AddConsoleExporter()
+                    .AddJaegerExporter()
+                    .AddAspNetCoreInstrumentation();
+                });
+            return services;
+        }
+    }
+}
