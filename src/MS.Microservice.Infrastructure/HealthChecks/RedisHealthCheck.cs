80e4e0cf1836b48f441aca895649c7402cbd3b59
marsonshine <ms27946@163.com>
2023-07-14 17:40:48 +0800

    feat: add sql.redis health check

---
+++ src/MS.Microservice.Infrastructure/HealthChecks/RedisHealthCheck.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MS.Microservice.Infrastructure.HealthChecks
+{
+    public class RedisHealthCheck : IHealthCheck
+    {
+        private readonly string _redisConnection;
+
+        public RedisHealthCheck(IConfiguration configuration)
+        {
+            _redisConnection = configuration.GetSection("Redis:ConnectionString").Get<string>();
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            return await Task.FromResult(HealthCheckResult.Healthy("The Redis connection is working"));
+        }
+    }
+}
---
+++ src/MS.Microservice.Infrastructure/HealthChecks/SqlHealthCheck.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using MySqlConnector;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MS.Microservice.Infrastructure.HealthChecks
+{
+    public class SqlHealthCheck : IHealthCheck
+    {
+        public const string Name = nameof(SqlHealthCheck);
+        private readonly string _connectionString;
+        public SqlHealthCheck(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("Default")!;
+        }
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                using var sqlConnection = new MySqlConnection(_connectionString);
+                await sqlConnection.OpenAsync(cancellationToken);
+                using var command = sqlConnection.CreateCommand();
+                command.CommandText = "SELECT 1";
+                await command.ExecuteScalarAsync(cancellationToken);
+                return HealthCheckResult.Healthy();
+            }
+            catch (Exception ex)
+            {
+                return HealthCheckResult.Unhealthy("数据库连接异常", exception: ex);
+            }
+        }
+    }
+}
