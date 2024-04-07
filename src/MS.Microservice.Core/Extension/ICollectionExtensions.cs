f1098aa77c5d0b0c46962074d0f3de34f414615a
marsonshine <ms27946@163.com>
2024-04-07 18:52:49 +0800

    feat: add extensions;intersectby,distinctdictionary,orderbyreference

--- src/MS.Microservice.Core/Extension/ICollectionExtensions.cs
+++ src/MS.Microservice.Core/Extension/ICollectionExtensions.cs
@@ -5,69 +5,132 @@ using System.Linq;
 
 namespace MS.Microservice.Core.Extension
 {
-    public static class ICollectionExtensions
-    {
-        public static bool IsNullOrEmpty<T>([MaybeNull] this ICollection<T> source)
-        {
-            return source == null || source.Count <= 0;
-        }
+	public static class ICollectionExtensions
+	{
+		public static bool IsNullOrEmpty<T>([MaybeNull] this ICollection<T> source)
+		{
+			return source == null || source.Count <= 0;
+		}
 
-        public static bool AddIfNotContains<T>([NotNull] this ICollection<T> source, T item)
-        {
-            Check.NotNull(source, nameof(source));
+		public static bool AddIfNotContains<T>([NotNull] this ICollection<T> source, T item)
+		{
+			Check.NotNull(source, nameof(source));
 
-            if (source.Contains(item))
-            {
-                return false;
-            }
+			if (source.Contains(item))
+			{
+				return false;
+			}
 
-            source.Add(item);
-            return true;
-        }
+			source.Add(item);
+			return true;
+		}
 
-        public static IEnumerable<T> AddIfNotContains<T>([NotNull] this ICollection<T> source, IEnumerable<T> items)
-        {
-            Check.NotNull(source, nameof(source));
+		public static IEnumerable<T> AddIfNotContains<T>([NotNull] this ICollection<T> source, IEnumerable<T> items)
+		{
+			Check.NotNull(source, nameof(source));
 
-            var addedItems = new List<T>();
+			var addedItems = new List<T>();
 
-            foreach (var item in items)
-            {
-                if (source.Contains(item))
-                {
-                    continue;
-                }
+			foreach (var item in items)
+			{
+				if (source.Contains(item))
+				{
+					continue;
+				}
 
-                source.Add(item);
-                addedItems.Add(item);
-            }
+				source.Add(item);
+				addedItems.Add(item);
+			}
 
-            return addedItems;
-        }
+			return addedItems;
+		}
 
-        public static IList<T> RemoveAll<T>([NotNull] this ICollection<T> source, Func<T, bool> predicate)
-        {
-            var items = source.Where(predicate).ToList();
+		public static IList<T> RemoveAll<T>([NotNull] this ICollection<T> source, Func<T, bool> predicate)
+		{
+			var items = source.Where(predicate).ToList();
 
-            foreach (var item in items)
-            {
-                source.Remove(item);
-            }
+			foreach (var item in items)
+			{
+				source.Remove(item);
+			}
 
-            return items;
-        }
+			return items;
+		}
 
-        public static bool ContainsAll<T>([NotNull] this ICollection<T> source, IEnumerable<T> items, IEqualityComparer<T> comparer)
-        {
-            Check.NotNull(source, nameof(source));
-            foreach (T item in items)
-            {
-                if (!source.Contains(item, comparer))
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-    }
+		public static bool ContainsAll<T>([NotNull] this ICollection<T> source, IEnumerable<T> items, IEqualityComparer<T> comparer)
+		{
+			Check.NotNull(source, nameof(source));
+			foreach (T item in items)
+			{
+				if (!source.Contains(item, comparer))
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public static IEnumerable<TSource1> IntersectBy<TSource1, TSource2, TKey>(
+			this IEnumerable<TSource1> first,
+			IEnumerable<TSource2> second,
+			Func<TSource1, TKey> firstKeySelector,
+			Func<TSource2, TKey> secondKeySelector)
+		{
+			HashSet<TKey> keys = new(second.Select(secondKeySelector));
+			foreach (var element in first)
+			{
+				if (keys.Contains(firstKeySelector(element)))
+				{
+					yield return element;
+				}
+			}
+		}
+
+		public static Dictionary<TKey, TValue> ToDistinctDictionary<TSource, TKey, TValue>(
+			this IEnumerable<TSource> source,
+			Func<TSource, TKey> keySelector,
+			Func<TSource, TValue> valueSelector)
+			where TKey : notnull
+		{
+			Dictionary<TKey, TValue> dictionary = new();
+			foreach (var element in source)
+			{
+				TKey key = keySelector(element);
+				if (!dictionary.ContainsKey(key))
+				{
+					dictionary.Add(key, valueSelector(element));
+				}
+			}
+			return dictionary;
+		}
+
+		public static IEnumerable<T> OrderByReference<T>(
+			this IEnumerable<T> target,
+			IEnumerable<T> reference)
+			where T : notnull
+		{
+			var indexMap = reference.Select((item, index) => new { item, index })
+									.ToDictionary(x => x.item, x => x.index);
+
+			return target.Where(indexMap.ContainsKey)
+						 .OrderBy(item => indexMap[item])
+						 .Concat(target.Where(item => !indexMap.ContainsKey(item)));
+		}
+
+		public static IEnumerable<TB> OrderByReference<TA, TB, TKey>(
+			this IEnumerable<TB> target,
+			IEnumerable<TA> reference,
+			Func<TA, TKey> referenceSelector,
+			Func<TB, TKey> targetSelector)
+			where TKey : notnull
+		{
+			var indexMap = reference.Select((item, index) => new { Key = referenceSelector(item), Index = index })
+									.Where(x => x.Key != null)
+									.ToDictionary(x => x.Key, x => x.Index);
+
+			return target.Where(item => indexMap.ContainsKey(targetSelector(item)))
+						 .OrderBy(item => indexMap[targetSelector(item)])
+						 .Concat(target.Where(item => !indexMap.ContainsKey(targetSelector(item))));
+		}
+	}
 }
---
+++ test/MS.Microservice.Core.Tests/Extensions/CollectionExtensionsTest.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MS.Microservice.Core.Extension;
+
+namespace MS.Microservice.Core.Tests.Extensions
+{
+	public class CollectionExtensionsTest
+	{
+		[Fact]
+		public void TestIntersectBy()
+		{
+			List<Person> people =
+			[
+				new Person { Name = "Alice", Age = 30 },
+				new Person { Name = "Bob", Age = 25 },
+				new Person { Name = "Charlie", Age = 35 }
+			];
+
+			List<Employee> employees =
+			[
+				new Employee { FirstName = "Alice", EmployeeId = 1 },
+				new Employee { FirstName = "David", EmployeeId = 2 }
+			];
+
+			// 使用 IntersectBy 查找名称相同的人和员工
+			var commonNames = people.IntersectBy(employees, person => person.Name, employee => employee.FirstName);
+			Assert.NotEmpty(commonNames);
+			Assert.Equal("Alice", commonNames.First().Name);
+		}
+
+		[Fact]
+		public void TestDistinctDictionary()
+		{
+			List<Person> people =
+			[
+				new Person { Name = "Alice", Age = 30 },
+				new Person { Name = "Bob", Age = 25 },
+				new Person { Name = "Alice", Age = 35 }
+			];
+
+			var peopleDictionary = people.ToDistinctDictionary(person => person.Name!, person => person.Age);
+			Assert.NotEmpty(peopleDictionary);
+			Assert.Equal(2, peopleDictionary.Count);
+		}
+
+		[Fact]
+		public void TestOrderByReference()
+		{
+			List<int> collectionA = [1, 2, 3, 4, 5];
+			List<int> collectionB = [5, 3, 1];
+
+			var orderedCollectionB = collectionB.OrderByReference(collectionA)
+				.ToList();
+
+			Assert.NotEmpty(orderedCollectionB);
+			Assert.Equal(1, orderedCollectionB[0]);
+			Assert.Equal(3, orderedCollectionB[1]);
+			Assert.Equal(5, orderedCollectionB[2]);
+		}
+
+		[Fact]
+		public void TestOrderByReference2()
+		{
+			List<Person> people = [
+				new Person { Name = "Alice" },
+				new Person { Name = "Bob" },
+				new Person { Name = "Charlie" }
+			];
+
+			List<Order> orders =
+			[
+				new Order { CustomerName = "Charlie" },
+				new Order { CustomerName = "Alice" }
+			];
+
+			var sortedOrders = orders.OrderByReference(people, person => person.Name!, order => order.CustomerName)
+				.ToList();
+
+			Assert.NotEmpty(sortedOrders);
+			Assert.Equal(2, sortedOrders.Count);
+			Assert.Equal("Alice", sortedOrders[0].CustomerName);
+			Assert.Equal("Charlie", sortedOrders[1].CustomerName);
+		}
+
+		class Person
+		{
+			public string? Name { get; set; }
+			public int Age { get; set; }
+		}
+
+		class Employee
+		{
+			public string? FirstName { get; set; }
+			public int EmployeeId { get; set; }
+		}
+
+		class Order
+		{
+			public string CustomerName { get; set; } = "";
+		}
+	}
+}
