b99d2687ab0e788cc593fc457a4e302132cf6b39
marsonshine <ms27946@163.com>
2024-04-15 16:18:54 +0800

    opt: md5 perf opts

--- src/MS.Microservice.Core/Security/Summary/Md5.cs
+++ src/MS.Microservice.Core/Security/Summary/Md5.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace MS.Microservice.Core.Security.Summary
 {
-    public static class Md5
+	public static class Md5
     {
         public static string Encrypt(string value, Encoding encoding)
         {
@@ -23,5 +23,24 @@ namespace MS.Microservice.Core.Security.Summary
             }
             return result.Replace("-", "");
         }
-    }
+
+        public static string Encrypt(ReadOnlySpan<byte> bytes) => ComputeMD5(bytes);
+
+		private static string ComputeMD5(ReadOnlySpan<byte> bytes)
+		{
+			Span<byte> hashBytes = stackalloc byte[16];
+			MD5.TryHashData(bytes, hashBytes, out int bytesWritten);
+			return BitConverter.ToString(hashBytes.ToArray());
+		}
+
+		private static string BytesToMD5String(ReadOnlySpan<byte> hashBytes)
+		{
+			StringBuilder builder = new(32);
+			foreach (byte b in hashBytes)
+			{
+				builder.AppendFormat("{0:x2}", b);
+			}
+			return builder.ToString();
+		}
+	}
 }
