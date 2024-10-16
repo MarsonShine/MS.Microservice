0333f52791b406b2952fc74300ffa7f48222a015
marsonshine <ms27946@163.com>
2024-10-16 11:09:07 +0800

    fix: rename result2 -> result

--- src/MS.Microservice.Core/Dto/Result.cs
+++ src/MS.Microservice.Core/Dto/Result.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
@@ -7,48 +8,19 @@ using System.Threading.Tasks;
 
 namespace MS.Microservice.Core.Dto
 {
-	public class Result<T>
-	{
-		private Result(T value)
-		{
-			IsSuccess = true;
-			Value = value;
-			Error = null;
-		}
-
-		private Result(Exception error)
-		{
-			IsSuccess = false;
-			Value = default;
-			Error = error;
-		}
-		[MemberNotNullWhen(true, nameof(Value))]
-		[MemberNotNullWhen(false, nameof(Error))]
-		public bool IsSuccess { get; private set; }
-
-		public T? Value { get; private set; }
-
-		public Exception? Error { get; private set; }
-
-		public static Result<T> Success(T value) => new(value);
-		public static Result<T> Fail(Exception error) => new(error);
-
-		public static implicit operator Result<T>(T value) => Success(value);
-	}
-
 	//https://andrewlock.net/working-with-the-result-pattern-part-1-replacing-exceptions-as-control-flow/#making-the-result-pattern-safer
-	public class Result2<T>
+	public class Result<T>
 	{
 		private readonly T? _value;
 		private readonly Exception? _error;
-		private Result2(T value)
+		private Result(T value)
 		{
 			IsSuccess = true;
 			_value = value;
 			_error = null;
 		}
 
-		private Result2(Exception error)
+		private Result(Exception error)
 		{
 			IsSuccess = false;
 			_value = default;
@@ -58,21 +30,22 @@ namespace MS.Microservice.Core.Dto
 		[MemberNotNullWhen(false, nameof(_error))]
 		public bool IsSuccess { get; private set; }
 		// https://github.com/dotnet/csharplang/blob/main/proposals/TypeUnions.md
-		public Result2<TReturn> Switch<TReturn>(Func<T, TReturn> onSuccess, Func<Exception, Exception> onFailure)
+		public Result<TReturn> Switch<TReturn>(Func<T, TReturn> onSuccess, Func<Exception, Exception> onFailure)
 		{
 			if (IsSuccess)
 			{
-				return Result2<TReturn>.Success(onSuccess(_value));
+				return Result<TReturn>.Success(onSuccess(_value));
 			}
 			else
 			{
-				return Result2<TReturn>.Fail(onFailure(_error));
+				var err = onFailure(_error);
+				return Result<TReturn>.Fail(err);
 			}
 		}
 
-		public static Result2<T> Success(T value) => new(value);
-		public static Result2<T> Fail(Exception error) => new(error);
+		public static Result<T> Success(T value) => new(value);
+		public static Result<T> Fail(Exception error) => new(error);
 
-		public static implicit operator Result2<T>(T value) => Success(value);
+		public static implicit operator Result<T>(T value) => Success(value);
 	}
 }
--- src/MS.Microservice.Core/Extension/ResultExtensions.cs
+++ src/MS.Microservice.Core/Extension/ResultExtensions.cs
@@ -5,26 +5,27 @@ namespace MS.Microservice.Core.Extension
 {
 	internal static class ResultExtensions
 	{
-		public static Result2<TResult> Select<TFrom,TResult>(
-			this Result2<TFrom> source, 
+		public static Result<TResult> Select<TFrom, TResult>(
+			this Result<TFrom> source,
 			Func<TFrom, TResult> selector)
 		{
 			return source.Switch(
 				onSuccess: r => selector(r),
-				onFailure: e => Result2<TResult>.Fail(e)); // 无法将Result<TResult> -> Exception
+				onFailure: e => Result<TResult>.Fail(e)); // 无法将Result<TResult> -> Exception
 		}
 
-		public static Result2<TResult> SelectMany<TSource,TMiddle,TResult>(
-			this Result2<TSource> source,
-			Func<TSource,Result2<TMiddle>> collectionSelector,
-			Func<TSource,TMiddle,TResult> resultSelector)
+		public static Result<TResult> SelectMany<TSource, TMiddle, TResult>(
+			this Result<TSource> source,
+			Func<TSource, Result<TMiddle>> collectionSelector,
+			Func<TSource, TMiddle, TResult> resultSelector)
 		{
 			return source.Switch(
-				onSuccess: r => { 
-					Result2<TMiddle> result = collectionSelector(r);
-					return result.Select(v => resultSelector(r, v));
+				onSuccess: r =>
+				{
+					Result<TMiddle> middleResult = collectionSelector(r);
+					return middleResult.Select(v => resultSelector(r,v));
 				},
-				onFailure: e => Result2<TResult>.Fail(e));
+				onFailure: e => Result<TResult>.Fail(e)); // 无法将Result<TResult> -> Exception
 		}
 	}
 }
