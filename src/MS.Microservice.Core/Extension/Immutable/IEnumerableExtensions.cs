3b9ec1a26bbbc9091e0296eca984092bcd7ab670
marsonshine <ms27946@163.com>
2025-04-08 17:03:17 +0800

    feat: add IEnumerableExtensions support record reference compare

---
+++ src/MS.Microservice.Core/Extension/Immutable/IEnumerableExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace MS.Microservice.Core.Extension
+{
+    public static partial class IEnumerableExtensions
+    {
+        public static ImmutableDictionary<TKey, TValue> ToImmutableReferenceDictionary<TSource, TKey, TValue>(
+        this IEnumerable<TSource> source,
+        Func<TSource, TKey> keySelector,
+        Func<TSource, TValue> valueSelector) where TKey : class
+        {
+            return source.ToImmutableDictionary(
+                keySelector,
+                valueSelector,
+                (IEqualityComparer<TKey>)ReferenceEqualityComparer.Instance);
+        }
+
+        public static ImmutableDictionary<TKey, TSource> ToImmutableReferenceDictionary<TSource, TKey>(
+        this IEnumerable<TSource> source,
+        Func<TSource, TKey> keySelector) where TKey : class
+        {
+            return source.ToImmutableDictionary(
+                keySelector,
+                (IEqualityComparer<TKey>)ReferenceEqualityComparer.Instance);
+        }
+
+        public static ImmutableDictionary<TKey, TValue> ToImmutableReferenceDictionary<TKey, TValue>(
+        this IDictionary<TKey, TValue> source) where TKey : class
+        {
+            return source.ToImmutableDictionary(
+                ReferenceEqualityComparer.Instance);
+        }
+
+        public static ImmutableDictionary<TKey, TValue> ToImmutableReferenceDictionary<TKey, TValue, TSourceValue>(
+        this IDictionary<TKey, TSourceValue> source,
+        Func<KeyValuePair<TKey, TSourceValue>, TValue> valueSelector) where TKey : class
+        {
+            return source.ToImmutableDictionary(
+                pair => pair.Key,
+                valueSelector,
+                (IEqualityComparer<TKey>)ReferenceEqualityComparer.Instance);
+        }
+
+        public static ImmutableDictionary<string, TValue> ToImmutableOrdinalDictionary<TSource, TValue>(
+            this IEnumerable<TSource> source,
+            Func<TSource, string> keySelector,
+            Func<TSource, TValue> valueSelector)
+        {
+            return source.ToImmutableDictionary(keySelector, valueSelector, StringComparer.Ordinal);
+        }
+    }
+}
