ccace6451ab6b20f37667b3cc9e563354bdc7571
marsonshine <ms27946@163.com>
2025-04-25 14:46:09 +0800

    feat: add StringBuilderCache better for StringBuilder

---
+++ src/MS.Microservice.Core/StringBuilderCache.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace MS.Microservice.Core
+{
+    /// <summary>
+    /// https://referencesource.microsoft.com/#mscorlib/system/text/stringbuildercache.cs
+    /// </summary>
+    public class StringBuilderCache
+    {
+        private const int MAX_BUILDER_SIZE = 360;
+        private const int DefaultCapacity = 16;
+        [ThreadStatic]
+        private static StringBuilder? CachedInstance;
+        public static StringBuilder Acquire(int capacity = DefaultCapacity)
+        {
+            if (capacity <= MAX_BUILDER_SIZE)
+            {
+                StringBuilder? sb = CachedInstance;
+                if (sb != null)
+                {
+                    // Avoid stringbuilder block fragmentation by getting a new StringBuilder
+                    // when the requested size is larger than the current capacity
+                    if (capacity <= sb.Capacity)
+                    {
+                        CachedInstance = null;
+                        sb.Clear();
+                        return sb;
+                    }
+                }
+            }
+            return new StringBuilder(capacity);
+        }
+        public static void Release(StringBuilder sb)
+        {
+            if (sb.Capacity <= MAX_BUILDER_SIZE)
+            {
+                CachedInstance = sb;
+            }
+        }
+
+        public static string GetStringAndRelease(StringBuilder sb)
+        {
+            string result = sb.ToString();
+            Release(sb);
+            return result;
+        }
+    }
+}
