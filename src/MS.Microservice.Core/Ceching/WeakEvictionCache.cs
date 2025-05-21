fd09f7b7863f479d7f6d182a6d644f80f37e5643
marsonshine <ms27946@163.com>
2025-05-21 16:56:00 +0800

    feat: add trong to weak helper

---
+++ src/MS.Microservice.Core/Ceching/WeakEvictionCache.cs
@@ -0,0 +1,68 @@
+﻿using MS.Microservice.Core.Microsoft.System;
+using System;
+using System.Collections.Generic;
+
+namespace MS.Microservice.Core.Ceching
+{
+    public class WeakEvictionCache<TKey, TValue> where TValue : class
+        where TKey : notnull
+    {
+        private readonly TimeSpan _weakEvictionThreshold;
+        private Dictionary<TKey, StrongToWeakReference<TValue>> _items;
+
+        public WeakEvictionCache(TimeSpan weakEvictionThreshold)
+        {
+            _weakEvictionThreshold = weakEvictionThreshold;
+            _items = [];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            _items.Add(key, new StrongToWeakReference<TValue>(value));
+        }
+
+        public bool TryGet(TKey key, out TValue? result)
+        {
+            result = null;
+            if (_items.TryGetValue(key, out var value))
+            {
+                result = value.Target!;
+                if (result != null)
+                {
+                    // 对象被使用时尝试恢复强引用  
+                    value.MakeStrong();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void DoWeakEviction()
+        {
+            List<TKey> toRemove = new List<TKey>();
+            foreach (var strongToWeakReference in _items)
+            {
+                var reference = strongToWeakReference.Value;
+                var target = reference.Target;
+                if (target != null)
+                {
+                    if (DateTime.Now.Subtract(reference.StrongTime) >= _weakEvictionThreshold)
+                    {
+                        reference.MakeWeak();
+                    }
+                }
+                else
+                {
+                    // 清除已失效的弱引用  
+                    toRemove.Add(strongToWeakReference.Key);
+                }
+            }
+
+            foreach (var key in toRemove)
+            {
+                _items.Remove(key);
+            }
+        }
+    }
+}
---
+++ src/MS.Microservice.Core/Microsoft/System/StrongToWeakReference.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MS.Microservice.Core.Microsoft.System
+{
+    /// <summary>
+    /// https://source.dot.net/#System.Net.Http/src/libraries/Common/src/System/StrongToWeakReference.cs
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class StrongToWeakReference<T> : WeakReference where T : class
+    {
+        private T? _strongRef;
+        private DateTime _strongTime;
+
+        /// <summary>Initializes the instance with a strong reference to the specified object.</summary>
+        /// <param name="obj">The object to wrap.</param>
+        public StrongToWeakReference(T obj) : base(obj)
+        {
+            Debug.Assert(obj != null, "Expected non-null obj");
+            _strongRef = obj;
+            _strongTime = DateTime.Now;
+        }
+
+        /// <summary>Drops the strong reference to the object, keeping only a weak reference.</summary>
+        public void MakeWeak() => _strongRef = null;
+
+        /// <summary>Restores the strong reference, assuming the object hasn't yet been collected.</summary>
+        public void MakeStrong()
+        {
+            _strongRef = WeakTarget;
+            Debug.Assert(_strongRef != null, $"Expected non-null {nameof(_strongRef)} after setting");
+            if (_strongRef != null)
+            {
+                _strongTime = DateTime.Now;
+            }
+        }
+
+        /// <summary>Gets the wrapped object.</summary>
+        public new T? Target => _strongRef ?? WeakTarget;
+
+        /// <summary>Gets the wrapped object via its weak reference.</summary>
+        private T? WeakTarget => base.Target as T;
+
+        public DateTime StrongTime { get => _strongTime; }
+    }
+}
